0445db4835deaa97d076ed6784117d089276d50f
Azim Gadzhiagayev <me@azimgd.com>
2016-10-16 23:03:31 +0500

    Fix router file to use IndexRoute

--- src/app/containers/HomeContainer.js
+++ src/app/containers/HomeContainer.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ProductsContainer from 'containers/ProductsContainer';
 
 const HomeContainer = () =>
   <div className="HomeContainerBlock">
-    <ProductsContainer />
   </div>;
 
 export default HomeContainer;
--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import LayoutContainer from 'containers/LayoutContainer';
 import LoginContainer from 'containers/LoginContainer';
-import HomeContainer from 'containers/HomeContainer';
+import ProductsContainer from 'containers/ProductsContainer';
 import ProductViewContainer from 'containers/ProductViewContainer';
 import CheckoutContainer from 'containers/CheckoutContainer';
 
 const Routes = () =>
   <Router history={browserHistory}>
     <Route path="/login" component={LoginContainer} />
-    <Route component={LayoutContainer}>
-      <Route path="/" component={HomeContainer} />
+    <Route path="/" component={LayoutContainer}>
+      <IndexRoute component={ProductsContainer} />
       <Route path="/products/">
+        <IndexRoute component={ProductsContainer} />
         <Route path=":id" component={ProductViewContainer} />
       </Route>
       <Route path="/checkout" component={CheckoutContainer} />
