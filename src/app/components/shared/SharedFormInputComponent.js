056caa63ca9fbb17d200aec1604d60788819d411
Azim Gadzhiagayev <me@azimgd.com>
2016-10-26 13:26:08 +0500

    ADD Yup validation, add validation for product create form

---
+++ src/app/components/shared/SharedFormInputComponent.js
@@ -0,0 +1,65 @@
+import React, { PropTypes } from 'react';
+import _ from 'lodash';
+
+/**
+ *
+ */
+export const InputText = ({ input, label, meta: { asyncValidating, touched, error } }) => (
+  <div className="SharedFormInputComponent SharedFormInputComponent--InputText">
+    <label>{label}</label>
+    <div className={asyncValidating ? 'SharedFormInputComponent-isValidating' : ''}>
+      <input {...input} type="text" placeholder={label} />
+      {touched && error && <div className="SharedFormInputComponent-error">{error}</div>}
+    </div>
+  </div>
+);
+
+InputText.propTypes = {
+  input: PropTypes.object,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  meta: PropTypes.object,
+};
+
+/**
+ *
+ */
+export const InputTextarea = ({ input, label, meta: { asyncValidating, touched, error } }) => (
+  <div className="SharedFormInputComponent SharedFormInputComponent--InputTextarea">
+    <label>{label}</label>
+    <div className={asyncValidating ? 'SharedFormInputComponent-isValidating' : ''}>
+      <textarea {...input} placeholder={label} />
+      {touched && error && <div className="SharedFormInputComponent-error">{error}</div>}
+    </div>
+  </div>
+);
+
+InputTextarea.propTypes = {
+  input: PropTypes.object,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  meta: PropTypes.object,
+};
+
+/**
+ *
+ */
+export const InputSelect = ({ input, label, options, meta: { asyncValidating, touched, error } }) => (
+  <div className="SharedFormInputComponent SharedFormInputComponent--InputSelect">
+    <label>{label}</label>
+    <div className={asyncValidating ? 'SharedFormInputComponent-isValidating' : ''}>
+      <select {...input} placeholder={label}>
+        {_.map(options, (item, key) => <option value={key} key={key}>{item}</option>)}
+      </select>
+      {touched && error && <div className="SharedFormInputComponent-error">{error}</div>}
+    </div>
+  </div>
+);
+
+InputSelect.propTypes = {
+  input: PropTypes.object,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  meta: PropTypes.object,
+  options: PropTypes.object,
+};
---
+++ src/app/validators/ProductFormComponentValidator.js
@@ -0,0 +1,25 @@
+import yup from 'yup';
+import { transformYupValidationErrors } from 'utils/index';
+
+const schema = yup.object().shape({
+  name: yup.string().email(),
+  email: yup.string().email().required(),
+  phone: yup.string().required(),
+  address: yup.string().required(),
+  city: yup.string().required(),
+  productTitle: yup.string().required(),
+  productCategory: yup.string().required(),
+  productSubCategory: yup.string().required(),
+  productDescription: yup.string().required(),
+  productPrice: yup.string().required(),
+});
+
+const asyncValidate = values => schema.validate(values, {
+  abortEarly: false,
+})
+.catch(res => { throw transformYupValidationErrors(res); });
+
+export default {
+  asyncBlurFields: [],
+  asyncValidate,
+};
