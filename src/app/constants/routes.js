dc523527dad83ec1dbb568996a20e0ede3b9765f
Azim Gadzhiagayev <me@azimgd.com>
2016-10-28 18:03:56 +0500

    Create named routes

---
+++ src/app/constants/routes.js
@@ -0,0 +1,9 @@
+export const ROOT = '/';
+export const LOGIN = '/login';
+
+export const PRODUCTS = '/products';
+export const PRODUCTS_SHOW = ':id';
+export const PRODUCTS_CREATE = '/form';
+
+export const CHECKOUT = '/checkout';
+export const NOTFOUND = '/404';
