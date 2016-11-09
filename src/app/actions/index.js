6065a51ad3408fd0e4c04ffd3bd432b3a6a8a127
Azim Gadzhiagayev <me@azimgd.com>
2016-11-09 17:57:49 +0500

    ADD Api calls, actions, constants for page sub categories and product sub categories, add separation comments

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -4,6 +4,9 @@ import * as constants from 'constants/index';
 export const toggleCartDropdown = createAction(constants.TOGGLE_CART_DROPDOWN);
 export const toggleAlertMessage = createAction(constants.TOGGLE_ALERT_MESSAGE);
 
+/**
+ * Pages
+ */
 export const getPagesRequest = createAction(constants.GET_PAGES_REQUEST);
 export const getPagesSuccess = createAction(constants.GET_PAGES_SUCCESS);
 export const getPagesFailure = createAction(constants.GET_PAGES_FAILURE);
@@ -16,6 +19,10 @@ export const postPageRequest = createAction(constants.POST_PAGE_REQUEST);
 export const postPageSuccess = createAction(constants.POST_PAGE_SUCCESS);
 export const postPageFailure = createAction(constants.POST_PAGE_FAILURE);
 
+
+/**
+ * Products
+ */
 export const getProductsRequest = createAction(constants.GET_PRODUCTS_REQUEST);
 export const getProductsSuccess = createAction(constants.GET_PRODUCTS_SUCCESS);
 export const getProductsFailure = createAction(constants.GET_PRODUCTS_FAILURE);
@@ -28,6 +35,10 @@ export const postProductRequest = createAction(constants.POST_PRODUCT_REQUEST);
 export const postProductSuccess = createAction(constants.POST_PRODUCT_SUCCESS);
 export const postProductFailure = createAction(constants.POST_PRODUCT_FAILURE);
 
+
+/**
+ * Page categories
+ */
 export const getPageCategoriesRequest = createAction(constants.GET_PAGE_CATEGORIES_REQUEST);
 export const getPageCategoriesSuccess = createAction(constants.GET_PAGE_CATEGORIES_SUCCESS);
 export const getPageCategoriesFailure = createAction(constants.GET_PAGE_CATEGORIES_FAILURE);
@@ -40,6 +51,26 @@ export const postPageCategoryRequest = createAction(constants.POST_PAGE_CATEGORY
 export const postPageCategorySuccess = createAction(constants.POST_PAGE_CATEGORY_SUCCESS);
 export const postPageCategoryFailure = createAction(constants.POST_PAGE_CATEGORY_FAILURE);
 
+
+/**
+ * Page categories
+ */
+export const getPageSubCategoriesRequest = createAction(constants.GET_PAGE_SUB_CATEGORIES_REQUEST);
+export const getPageSubCategoriesSuccess = createAction(constants.GET_PAGE_SUB_CATEGORIES_SUCCESS);
+export const getPageSubCategoriesFailure = createAction(constants.GET_PAGE_SUB_CATEGORIES_FAILURE);
+
+export const getPageSubCategoryRequest = createAction(constants.GET_PAGE_SUB_CATEGORY_REQUEST);
+export const getPageSubCategorySuccess = createAction(constants.GET_PAGE_SUB_CATEGORY_SUCCESS);
+export const getPageSubCategoryFailure = createAction(constants.GET_PAGE_SUB_CATEGORY_FAILURE);
+
+export const postPageSubCategoryRequest = createAction(constants.POST_PAGE_SUB_CATEGORY_REQUEST);
+export const postPageSubCategorySuccess = createAction(constants.POST_PAGE_SUB_CATEGORY_SUCCESS);
+export const postPageSubCategoryFailure = createAction(constants.POST_PAGE_SUB_CATEGORY_FAILURE);
+
+
+/**
+ * Product categories
+ */
 export const getProductCategoriesRequest = createAction(constants.GET_PRODUCT_CATEGORIES_REQUEST);
 export const getProductCategoriesSuccess = createAction(constants.GET_PRODUCT_CATEGORIES_SUCCESS);
 export const getProductCategoriesFailure = createAction(constants.GET_PRODUCT_CATEGORIES_FAILURE);
@@ -51,3 +82,19 @@ export const getProductCategoryFailure = createAction(constants.GET_PRODUCT_CATE
 export const postProductCategoryRequest = createAction(constants.POST_PRODUCT_CATEGORY_REQUEST);
 export const postProductCategorySuccess = createAction(constants.POST_PRODUCT_CATEGORY_SUCCESS);
 export const postProductCategoryFailure = createAction(constants.POST_PRODUCT_CATEGORY_FAILURE);
+
+
+/**
+ * Product sub categories
+ */
+export const getProductSubCategoriesRequest = createAction(constants.GET_PRODUCT_SUB_CATEGORIES_REQUEST);
+export const getProductSubCategoriesSuccess = createAction(constants.GET_PRODUCT_SUB_CATEGORIES_SUCCESS);
+export const getProductSubCategoriesFailure = createAction(constants.GET_PRODUCT_SUB_CATEGORIES_FAILURE);
+
+export const getProductSubCategoryRequest = createAction(constants.GET_PRODUCT_SUB_CATEGORY_REQUEST);
+export const getProductSubCategorySuccess = createAction(constants.GET_PRODUCT_SUB_CATEGORY_SUCCESS);
+export const getProductSubCategoryFailure = createAction(constants.GET_PRODUCT_SUB_CATEGORY_FAILURE);
+
+export const postProductSubCategoryRequest = createAction(constants.POST_PRODUCT_SUB_CATEGORY_REQUEST);
+export const postProductSubCategorySuccess = createAction(constants.POST_PRODUCT_SUB_CATEGORY_SUCCESS);
+export const postProductSubCategoryFailure = createAction(constants.POST_PRODUCT_SUB_CATEGORY_FAILURE);
--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+/**
+ * Pages
+ */
 export const getPages = () =>
   axios.get('//localhost:8080/api/pages')
   .then(res => res.data);
@@ -12,6 +15,10 @@ export const postPage = (data) =>
   axios.post('//localhost:8080/api/pages', data)
   .then(res => res.data);
 
+
+/**
+ * Products
+ */
 export const getProducts = () =>
   axios.get('//localhost:8080/api/products')
   .then(res => res.data);
@@ -24,6 +31,10 @@ export const postProduct = (data) =>
   axios.post('//localhost:8080/api/products', data)
   .then(res => res.data);
 
+
+/**
+ * Page categories
+ */
 export const getPageCategories = () =>
   axios.get('//localhost:8080/api/pageCategories')
   .then(res => res.data);
@@ -36,6 +47,26 @@ export const postPageCategory = (data) =>
   axios.post('//localhost:8080/api/pageCategories', data)
   .then(res => res.data);
 
+
+/**
+ * Page sub categories
+ */
+export const getPageSubCategories = () =>
+  axios.get('//localhost:8080/api/pageSubCategories')
+  .then(res => res.data);
+
+export const getPageSubCategory = ({ id }) =>
+  axios.get(`//localhost:8080/api/pageSubCategories/${id}`)
+  .then(res => res.data);
+
+export const postPageSubCategory = (data) =>
+  axios.post('//localhost:8080/api/pageSubCategories', data)
+  .then(res => res.data);
+
+
+/**
+ * Product categories
+ */
 export const getProductCategories = () =>
   axios.get('//localhost:8080/api/productCategories')
   .then(res => res.data);
@@ -46,4 +77,20 @@ export const getProductCategory = ({ id }) =>
 
 export const postProductCategory = (data) =>
   axios.post('//localhost:8080/api/productCategories', data)
+  .then(res => res.data);
+
+
+/**
+ * Product sub categories
+ */
+export const getProductSubCategories = () =>
+  axios.get('//localhost:8080/api/productSubCategories')
+  .then(res => res.data);
+
+export const getProductSubCategory = ({ id }) =>
+  axios.get(`//localhost:8080/api/productSubCategories/${id}`)
+  .then(res => res.data);
+
+export const postProductSubCategory = (data) =>
+  axios.post('//localhost:8080/api/productSubCategories', data)
   .then(res => res.data);
