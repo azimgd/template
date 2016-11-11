85676820bdea555e9aca5274ed79635ce8152b2e
Azim Gadzhiagayev <me@azimgd.com>
2016-11-11 22:38:15 +0500

    Add temporary topbar navigation links

--- src/app/components/topnavbar/TopnavbarComponent.js
+++ src/app/components/topnavbar/TopnavbarComponent.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const TopnavbarComponent = () =>
   <div className="TopnavbarComponentBlock">
     <div className="TopnavbarComponent">
       <ul className="TopnavbarComponent-nav">
-        <li className="TopnavbarComponent-nav-item"><a href="">Home page</a></li>
-        <li className="TopnavbarComponent-nav-item"><a href="">Products page</a></li>
-        <li className="TopnavbarComponent-nav-item"><a href="">Discounts page</a></li>
-        <li className="TopnavbarComponent-nav-item"><a href="">Daily offer</a></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/">Home page</Link></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/products">All products</Link></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/products/new">Create new product</Link></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/pages">All pages</Link></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/pages/new">Create new page</Link></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/config/categories/products">Product categories</Link></li>
+        <li className="TopnavbarComponent-nav-item"><Link to="/config/categories/pages">Page categories</Link></li>
       </ul>
     </div>
   </div>;
