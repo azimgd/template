289cdb020f46a8c25902883091968f919405b838
Taras <ter4444@gmail.com>
2017-07-20 23:50:27 +0700

    added transform-object-assign package required for slick carousel functionality

--- webpack.config.js
+++ webpack.config.js
@@ -151,6 +151,21 @@ module.exports = {
         include: /flexboxgrid/
       },
       {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }, {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           use: [{
